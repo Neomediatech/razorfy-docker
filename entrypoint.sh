50c6c9c54894278778200762737494f535e46c19
Dario B <feddar@users.noreply.github.com>
2020-10-12 13:36:49 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#display environment variables passed with --env
+echo "\$RAZORFY_DEBUG= $RAZORFY_DEBUG"
+echo "\$RAZORFY_BINDPORT= $RAZORFY_BINDPORT"
+echo "\$RAZORFY_MAXTHREADS= $RAZORFY_MAXTHREADS"
+echo "\$TZ= $TZ"
+echo
+
+NME=razor
+
+echo "export RAZORFY_BINDADDRESS=0.0.0.0" > /home/"$NME"/.profile
+[ -n "$RAZORFY_DEBUG" ] && echo "export RAZORFY_DEBUG=$RAZORFY_DEBUG" >> /home/"$NME"/.profile
+[ -n "$RAZORFY_BINDPORT" ] &&  echo "export RAZORFY_BINDPORT=$RAZORFY_BINDPORT" >> /home/"$NME"/.profile
+[ -n "$RAZORFY_MAXTHREADS" ] &&  echo "export RAZORFY_MAXTHREADS=$RAZORFY_MAXTHREADS" >> /home/"$NME"/.profile
+
+echo "Starting razorfy at $(date +'%x %X')"
+echo "Changing to user $NME"
+su -c '/usr/local/bin/razorfy.pl' - "$NME"
