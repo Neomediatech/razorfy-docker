98804a17312b748936502b966835dc82155947e9
Dario B <feddar@users.noreply.github.com>
2020-10-12 17:32:10 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,9 @@
 echo "\$RAZORFY_DEBUG= $RAZORFY_DEBUG"
 echo "\$RAZORFY_BINDPORT= $RAZORFY_BINDPORT"
 echo "\$RAZORFY_MAXTHREADS= $RAZORFY_MAXTHREADS"
-echo "\$TZ= $TZ"
 echo
 
 NME=razor
-TZ=${TZ:-"Europe/Rome"}
-cp /usr/share/zoneinfo/"$TZ" /etc/localtime
-echo "$TZ" > /etc/timezone
     
 echo "export RAZORFY_BINDADDRESS=0.0.0.0" > /home/"$NME"/.profile
 [ -n "$RAZORFY_DEBUG" ] && echo "export RAZORFY_DEBUG=$RAZORFY_DEBUG" >> /home/"$NME"/.profile
